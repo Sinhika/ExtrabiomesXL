0eebbe0dc78e99b0159fadf53b00f59fff4b9f47
Sinhika <sinhika@republicofnewhome.org>
2023-09-05 22:49:27 -0500

    added skeletal config file

---
+++ src/main/java/net/extrabiomes/config/ExtrabiomesConfig.java
@@ -0,0 +1,14 @@
+package net.extrabiomes.config;
+
+import mod.alexndr.simplecorelib.api.config.SimpleConfig;
+
+public class ExtrabiomesConfig extends SimpleConfig 
+{
+	public static ExtrabiomesConfig INSTANCE = new ExtrabiomesConfig();
+	
+	public ExtrabiomesConfig() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+} // end-class
