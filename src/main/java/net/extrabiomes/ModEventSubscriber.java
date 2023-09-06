b64b1f1303c17b475e22312092ee40364eb7e6ff
Sinhika <sinhika@republicofnewhome.org>
2023-09-06 13:22:18 -0500

    added ItemBlock registration to ModEventSubscriber

--- src/main/java/net/extrabiomes/ModEventSubscriber.java
+++ src/main/java/net/extrabiomes/ModEventSubscriber.java
@@ -7,11 +7,18 @@ import org.apache.logging.log4j.Logger;
 
 import net.extrabiomes.config.ConfigHelper;
 import net.extrabiomes.config.ConfigHolder;
+import net.extrabiomes.init.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
 
 @EventBusSubscriber(modid = ExtrabiomesXS.MODID, bus = MOD)
 public final class ModEventSubscriber 
@@ -27,6 +34,33 @@ public final class ModEventSubscriber
         LOGGER.debug("Common setup done");
     } // end onCommonSetup
 
+	/**
+	 * This method will be called by Forge when it is time for the mod to register its Items.
+	 * This method will always be called after the Block registry method.
+	 */
+    @SubscribeEvent
+    public static void onRegisterItems(final RegisterEvent event) 
+    {
+        if (event.getRegistryKey() == Registries.ITEM)
+        {
+	         // Automatically register BlockItems for all our Blocks
+	        ModBlocks.BLOCKS.getEntries().stream()
+	                .map(RegistryObject::get)
+	                // You can do extra filtering here if you don't want some blocks to have an BlockItem automatically registered for them
+	                //.filter(block -> needsItemBlock(block))
+	                // Register the BlockItem for the block
+	                .forEach(block -> {
+	                    // Create the new BlockItem with the block and it's properties
+	                    final BlockItem blockItem = new BlockItem(block, new Item.Properties());
+	                    // Register the BlockItem
+	                    event.register(Registries.ITEM,  helper -> {
+	                        helper.register(ForgeRegistries.BLOCKS.getKey(block), blockItem);
+	                    });
+	                });
+	        LOGGER.debug("Registered BlockItems");
+        }
+    } // end onRegisterItems
+    
     @SubscribeEvent
     public static void onModConfigEvent(final ModConfigEvent event)
     {
