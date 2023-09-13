ee185b2e369a71c201acd4660b9bd5a72e1558ec
Sinhika <sinhika@republicofnewhome.org>
2023-09-13 18:27:14 -0500

    tweaked voxel shapes

--- src/main/java/net/extrabiomes/content/StrawberryBlock.java
+++ src/main/java/net/extrabiomes/content/StrawberryBlock.java
@@ -12,6 +12,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class StrawberryBlock extends CropBlock 
 {
+	// potato shape
 	private static final VoxelShape[] SHAPE_BY_AGE = 
 			new VoxelShape[]{Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), 
 							 Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D), 
