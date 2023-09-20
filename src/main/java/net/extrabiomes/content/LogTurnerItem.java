0688653c75cb262f9af7e80445819c15002ec445
Sinhika <sinhika@republicofnewhome.org>
2023-09-19 20:40:49 -0500

    added functionality to log turner

--- src/main/java/net/extrabiomes/content/LogTurnerItem.java
+++ src/main/java/net/extrabiomes/content/LogTurnerItem.java
@@ -2,18 +2,24 @@ package net.extrabiomes.content;
 
 import java.util.List;
 
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 public class LogTurnerItem extends TieredItem 
 {
@@ -32,14 +38,27 @@ public class LogTurnerItem extends TieredItem
 	     Level level = pContext.getLevel();
 	     BlockPos blockpos = pContext.getClickedPos();
 	     BlockState blockstate = level.getBlockState(blockpos);
-	     Block block = blockstate.getBlock();
 	     
-	     
-	      
-		// TODO Auto-generated method stub
-		return super.useOn(pContext);
-	}
-
+	     if (blockstate.is(BlockTags.LOGS) )
+	     {
+	    	 Player player = pContext.getPlayer();
+	         ItemStack itemstack = pContext.getItemInHand();
+	    	 if (player.mayUseItemAt(blockpos, pContext.getHorizontalDirection(), itemstack))
+	    	 {
+	             if (player instanceof ServerPlayer) {
+	                 CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer)player, blockpos, itemstack);
+	              }
+	             
+	             level.playSound(player, blockpos, SoundEvents.WOOD_STEP, SoundSource.BLOCKS, 0.5F, 1.55F);
+	              BlockState blockstate2 = blockstate.rotate(level, blockpos, Rotation.CLOCKWISE_90);
+	              level.setBlockAndUpdate(blockpos, blockstate2);
+	              level.gameEvent(GameEvent.BLOCK_CHANGE, blockpos, GameEvent.Context.of(pContext.getPlayer(), blockstate2));
+	              
+	              return InteractionResult.sidedSuccess(level.isClientSide);
+	    	 } // end-if player.mayUseItemAt()
+	     } // end-if
+    	 return super.useOn(pContext);
+	} // end useOn;
 
 	@Override
 	public void appendHoverText(ItemStack pStack, Level pLevel, List<Component> pTooltipComponents,
