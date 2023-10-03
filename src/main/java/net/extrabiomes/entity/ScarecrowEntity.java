92cd64516cbe8ccb02c3a9453c1f75ff33214090
Sinhika <sinhika@republicofnewhome.org>
2023-10-02 20:15:57 -0500

    moved redwood stuff together in lang json

--- src/main/java/net/extrabiomes/entity/ScarecrowEntity.java
+++ src/main/java/net/extrabiomes/entity/ScarecrowEntity.java
@@ -7,7 +7,6 @@ import javax.annotation.Nullable;
 
 import net.extrabiomes.init.ModEntities;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
