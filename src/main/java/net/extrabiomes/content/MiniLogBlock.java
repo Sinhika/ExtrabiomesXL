4090d58c0a1364604c6a10e12379a848712c2116
Sinhika <sinhika@republicofnewhome.org>
2023-10-08 11:10:28 -0500

    rethink sakura logs as pipe blocks (chorus_plant)

--- src/main/java/net/extrabiomes/content/MiniLogBlock.java
+++ src/main/java/net/extrabiomes/content/MiniLogBlock.java
@@ -7,21 +7,24 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.WallBlock;
+import net.minecraft.world.level.block.PipeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.ToolAction;
 import net.minecraftforge.common.ToolActions;
 
-public class MiniLogBlock extends WallBlock 
+public class MiniLogBlock extends PipeBlock 
 {
 	protected final String tooltipKey;
-
+	protected final static float Apothem = 0.3125F;
+	
 	public MiniLogBlock(Properties pProperties, String toolTip) 
 	{
-		super(pProperties);
+		super(Apothem, pProperties);
 		this.tooltipKey = toolTip;
-	}
 
+	} // end ctor
+
+	
    @Override
     public BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction,
             boolean simulate)
