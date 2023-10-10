88351682cdac9cfc88d057d68edd5e15b6453660
Sinhika <sinhika@republicofnewhome.org>
2023-10-10 10:33:32 -0500

    fixed default states, added sideways shape. make PipeBlock.shapeByIndex not final so child class can change it

--- src/main/java/net/extrabiomes/content/MiniLogBlock.java
+++ src/main/java/net/extrabiomes/content/MiniLogBlock.java
@@ -2,15 +2,22 @@ package net.extrabiomes.content;
 
 import java.util.List;
 
+import net.extrabiomes.init.ModBlocks;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.PipeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.common.ToolAction;
 import net.minecraftforge.common.ToolActions;
 
@@ -22,6 +29,7 @@ public class MiniLogBlock extends PipeBlock
 	public MiniLogBlock(Properties pProperties, String toolTip) 
 	{
 		super(Apothem, pProperties);
+		this.shapeByIndex = this.makeLogShapes(Apothem);
         this.registerDefaultState(this.stateDefinition.any().setValue(NORTH, Boolean.valueOf(false))
         		.setValue(EAST, Boolean.valueOf(false)).setValue(SOUTH, Boolean.valueOf(false))
         		.setValue(WEST, Boolean.valueOf(false)).setValue(UP, Boolean.valueOf(false))
@@ -31,10 +39,50 @@ public class MiniLogBlock extends PipeBlock
 
 	} // end ctor
 
+	private VoxelShape[] makeLogShapes(float pApothem)
+	{
+		
+	}
+	
+	
+	@Override
+	public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) 
+	{
+		// TODO Auto-generated method stub
+		return super.getShape(pState, pLevel, pPos, pContext);
+	}
+
+
+	@Override
    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) 
    {
 	      pBuilder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN);
    }
+
+   @Override
+   public BlockState getStateForPlacement(BlockPlaceContext pContext) 
+   {
+	      return this.getStateForPlacement(pContext.getLevel(), pContext.getClickedPos());
+   }
+
+   public BlockState getStateForPlacement(BlockGetter pLevel, BlockPos pPos) 
+   {
+	   LeavesBlock myLeafBlock = ModBlocks.leaves_sakura.get();
+	   
+		BlockState blockstate = pLevel.getBlockState(pPos.below());
+		BlockState blockstate1 = pLevel.getBlockState(pPos.above());
+		BlockState blockstate2 = pLevel.getBlockState(pPos.north());
+		BlockState blockstate3 = pLevel.getBlockState(pPos.east());
+		BlockState blockstate4 = pLevel.getBlockState(pPos.south());
+		BlockState blockstate5 = pLevel.getBlockState(pPos.west());
+		return this.defaultBlockState()
+				.setValue(DOWN,Boolean.valueOf(blockstate.is(this)))
+				.setValue(UP, Boolean.valueOf(blockstate1.is(this) || blockstate1.is(myLeafBlock)))
+				.setValue(NORTH, Boolean.valueOf(blockstate2.is(this) || blockstate2.is(myLeafBlock)))
+				.setValue(EAST, Boolean.valueOf(blockstate3.is(this) || blockstate3.is(myLeafBlock)))
+				.setValue(SOUTH, Boolean.valueOf(blockstate4.is(this) || blockstate4.is(myLeafBlock)))
+				.setValue(WEST, Boolean.valueOf(blockstate5.is(this) || blockstate5.is(myLeafBlock)));
+   } // end getStateForPlacement
 
    @Override
     public BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction,
@@ -62,5 +110,10 @@ public class MiniLogBlock extends PipeBlock
 		pTooltip.add(Component.translatable(tooltipKey));
 	}
 
+	@Override
+   public boolean isPathfindable(BlockState pState, BlockGetter pLevel, BlockPos pPos, PathComputationType pType) 
+   {
+	   return false;
+   }
 	
 } // end-class
