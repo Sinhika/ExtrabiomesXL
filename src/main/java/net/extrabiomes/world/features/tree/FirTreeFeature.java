157e4e05eb7ea79847d8044691751dfb33ab6cae
Sinhika <sinhika@republicofnewhome.org>
2023-11-03 14:19:48 -0500

    rolled back refactoring of FirTreeFeatures, because it was broken.

--- src/main/java/net/extrabiomes/world/features/tree/FirTreeFeature.java
+++ src/main/java/net/extrabiomes/world/features/tree/FirTreeFeature.java
@@ -3,17 +3,15 @@ package net.extrabiomes.world.features.tree;
 import com.mojang.serialization.Codec;
 import net.extrabiomes.world.features.configuration.EBTreeConfiguration;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.TreeFeature;
 
-public class FirTreeFeature extends EBBaseTreeFeature
+public class FirTreeFeature extends Feature<EBTreeConfiguration>
 {
-    int CANOPY_RADIUS;
-    int MIN_LEAF_HEIGHT;
-
     public FirTreeFeature(Codec<EBTreeConfiguration> pCodec)
     {
         super(pCodec);
@@ -28,9 +26,12 @@ public class FirTreeFeature extends EBBaseTreeFeature
      */
     @Override public boolean place(FeaturePlaceContext<EBTreeConfiguration> pContext)
     {
-        boolean place = super.place(pContext);
+        EBTreeConfiguration treeConfig = pContext.config();
         BlockPos pos = pContext.origin().immutable();
-        int max_tree_altitude = pos.getY() + actual_height + 1;
+        RandomSource sourceRand = pContext.random();
+        WorldGenLevel level = pContext.level();
+        int height = treeConfig.base_height + sourceRand.nextInt(treeConfig.base_height_variance);
+        int max_tree_altitude = pos.getY() + height + 1;
 
         // height check
         if (pos.getY() < level.getMinBuildHeight() + 1 || max_tree_altitude > level.getMaxBuildHeight())
@@ -39,20 +40,20 @@ public class FirTreeFeature extends EBBaseTreeFeature
         }
 
         // min_leaf_height - was 1+rand(12)
-        MIN_LEAF_HEIGHT = treeConfig.canopy_start_height + sourceRand.nextInt(treeConfig.canopy_start_variance);
+        final int min_leaf_height = treeConfig.canopy_start_height + sourceRand.nextInt(treeConfig.canopy_start_variance);
         // canopy_radius - was 2+rand(6)
-        CANOPY_RADIUS = Math.min((treeConfig.canopy_width + sourceRand.nextInt(treeConfig.canopy_width_variance)), 6);
+        final int canopy_radius = Math.min((treeConfig.canopy_width + sourceRand.nextInt(treeConfig.canopy_width_variance)), 6);
 
         // make sure space for tree is clear or replaceable.
         BlockPos.MutableBlockPos checkpos = pos.mutable();
         for (int i1 = pos.getY(); i1 <= max_tree_altitude; i1++)
         {
             int k1;  // current canopy radius
-            if (i1 - pos.getY() < MIN_LEAF_HEIGHT) {
+            if (i1 - pos.getY() < min_leaf_height) {
                 k1 = 0;
             }
             else {
-                k1 = CANOPY_RADIUS;
+                k1 = canopy_radius;
             }
 
             for (int x1 = pos.getX() - k1; x1 <= pos.getX() + k1; x1++)
@@ -69,68 +70,11 @@ public class FirTreeFeature extends EBBaseTreeFeature
         } // end for i1
 
         // START PLACING BLOCKS
-        // place the 'dirt' block.
-        this.setBlock(level, pos.below(), treeConfig.dirt_provider.getState(sourceRand, pos.below()));
-
-        // stick some leaves up.
-        generateCanopy(level, sourceRand, pos, actual_height - MIN_LEAF_HEIGHT, CANOPY_RADIUS,
-                        treeConfig.foliage_provider.getState(sourceRand, pos));
-
-        // TODO: what are these variables?
-        final int j3 = sourceRand.nextInt(3); // TODO: do these constants need to be config VARIABLES?
-
-        // place trunk blocks.
-        generateBranches(level, sourceRand, pos, actual_height - j3, 0.0);
-
-        return true;
-    } // end place
-
-    /**
-     * Actually place the branches.
-     *
-     * @param world     current LevalAccessor
-     * @param rand      current RandomSource
-     * @param pos       BlockPos of tree origin
-     * @param height    canopy height  = actual_height - MIN_LEAF_HEIGHT
-     * @param radius    canopy radius
-     */
-    @Override public boolean generateBranches(LevelAccessor world, RandomSource rand, BlockPos pos, int height,
-                                              double radius)
-    {
-
-        // place trunk blocks.
         BlockPos.MutableBlockPos placePos = pos.mutable();
-        placePos.set(pos.getX(), pos.getY(), pos.getZ());
 
-        for (int l3 = 0; l3 < height; l3++)
-        {
-            // final Block block = world.getBlock(x, y + l3, z);
-            placePos.setY(pos.getY()+l3);
-
-            //if (block == null || block.isLeaves(world, x, y + l3, z) || block.isAir(world, x, y + l3, z) )
-            if (TreeFeature.validTreePos(level, placePos))
-            {
-                this.setBlock(world, placePos, treeConfig.trunk_provider.getState(sourceRand, placePos));
-            }
-        } // end-for l3
-
-        return true;
-    } // end generateBranches()
-
-    /**
-     * Generate the tree's canopy.
-     *
-     * @param world  LevelAccessor
-     * @param rand   a RandomSource
-     * @param pos    BlockPos of center of canopy
-     * @param radius canopy radius
-     * @param height canopy height
-     * @param leaves leaf BlockState
-     */
-    @Override public void generateCanopy(LevelAccessor world, RandomSource rand, BlockPos pos, double radius,
-                                         int height, BlockState leaves)
-    {
-        BlockPos.MutableBlockPos branchpos = pos.mutable();
+        // place the 'dirt' block.
+        placePos.move(Direction.DOWN);
+        level.setBlock(placePos, treeConfig.dirt_provider.getState(sourceRand, placePos), 3);
 
         // num block below treetop that trunk stops.
         int l1 = sourceRand.nextInt(2);   // TODO: do these constants need to be config VARIABLES?
@@ -139,9 +83,9 @@ public class FirTreeFeature extends EBBaseTreeFeature
         boolean flag1 = false;
 
         // stick some leaves up.
-        for (int i3 = 0; i3 <= height; i3++)
+        for (int i3 = 0; i3 <= height - min_leaf_height; i3++)
         {
-            final int k3 = pos.getY() + actual_height - i3;
+            final int k3 = pos.getY() + height - i3;
 
             for (int i4 = pos.getX() - l1; i4 <= pos.getX() + l1; i4++)
             {
@@ -150,11 +94,11 @@ public class FirTreeFeature extends EBBaseTreeFeature
                 for (int l4 = pos.getZ() - l1; l4 <= pos.getZ() + l1; l4++)
                 {
                     final int i5 = l4 - pos.getZ();
-                    branchpos.set(i4, k3, l4);
+                    placePos.set(i4, k3, l4);
                     if ((Math.abs(k4) != l1 || Math.abs(i5) != l1 || l1 <= 0)
-                            && (TreeFeature.isAirOrLeaves(world, branchpos)))
+                            && (TreeFeature.isAirOrLeaves(level, placePos)))
                     {
-                        this.setBlock(world, branchpos, leaves);
+                        level.setBlock(placePos, treeConfig.foliage_provider.getState(sourceRand, placePos), 3);
                     }
                 }  // end-for l4
             } // end-for i4
@@ -164,14 +108,34 @@ public class FirTreeFeature extends EBBaseTreeFeature
                 l1 = flag1 ? 1 : 0;
                 flag1 = true;
 
-                if (++j2 > radius)
-                    j2 = (int) radius;
+                if (++j2 > canopy_radius)
+                    j2 = canopy_radius;
             }
             else
             {
                 l1++;
             }
         } // end-for i3
-    } // end generateCanopy()
+
+        // TODO: what are these variables?
+        final int j3 = sourceRand.nextInt(3); // TODO: do these constants need to be config VARIABLES?
+
+        // place trunk blocks.
+        placePos.set(pos.getX(), pos.getY(), pos.getZ());
+
+        for (int l3 = 0; l3 < height - j3; l3++)
+        {
+            // final Block block = world.getBlock(x, y + l3, z);
+            placePos.setY(pos.getY()+l3);
+
+            //if (block == null || block.isLeaves(world, x, y + l3, z) || block.isAir(world, x, y + l3, z) )
+            if (TreeFeature.validTreePos(level, placePos))
+            {
+                level.setBlock(placePos, treeConfig.trunk_provider.getState(sourceRand, placePos), 3);
+            }
+        }
+
+        return true;
+    } // end place
 
 } // end class
--- src/main/java/net/extrabiomes/world/features/tree/MegaFirTreeFeature.java
+++ src/main/java/net/extrabiomes/world/features/tree/MegaFirTreeFeature.java
@@ -5,14 +5,13 @@ import net.extrabiomes.world.features.configuration.EBTreeConfiguration;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.TreeFeature;
 
-public class MegaFirTreeFeature extends EBBaseTreeFeature
+public class MegaFirTreeFeature extends Feature<EBTreeConfiguration>
 {
     public MegaFirTreeFeature(Codec<EBTreeConfiguration> pCodec)
     {
@@ -28,10 +27,13 @@ public class MegaFirTreeFeature extends EBBaseTreeFeature
      */
     @Override public boolean place(FeaturePlaceContext<EBTreeConfiguration> pContext)
     {
-        boolean place = super.place(pContext);
-
+        EBTreeConfiguration treeConfig = pContext.config();
         BlockPos pos = pContext.origin().immutable();
-        int max_tree_altitude = pos.getY() + actual_height + 1;
+        RandomSource sourceRand = pContext.random();
+        WorldGenLevel level = pContext.level();
+
+        final int height = treeConfig.base_height + sourceRand.nextInt(treeConfig.base_height_variance);
+        int max_tree_altitude = pos.getY() + height + 1;
 
         // height check
         if (pos.getY() < level.getMinBuildHeight() + 1 || max_tree_altitude > level.getMaxBuildHeight())
@@ -42,7 +44,7 @@ public class MegaFirTreeFeature extends EBBaseTreeFeature
         // probably min_leaf_height - was 1 + rand(12)
         final int min_leaf_height = treeConfig.canopy_start_height + sourceRand.nextInt(treeConfig.canopy_start_variance);
         // probably depth of canopy
-        final int canopy_depth = actual_height - min_leaf_height;
+        final int canopy_depth = height - min_leaf_height;
         // canopy radius -- originally 2+rand(9). Clamp to total 6.
         final int canopy_radius = Math.min((treeConfig.canopy_width + sourceRand.nextInt(treeConfig.canopy_width_variance)), 6);
 
@@ -70,42 +72,80 @@ public class MegaFirTreeFeature extends EBBaseTreeFeature
 
         // START PLACING BLOCKS
         BlockPos.MutableBlockPos placePos = pos.mutable();
-        BlockState dirt = treeConfig.dirt_provider.getState(sourceRand, placePos);
 
         // place the 'dirt' block.
         placePos.move(Direction.DOWN);
-        this.setBlock(level, placePos, dirt);
+        level.setBlock(placePos, treeConfig.dirt_provider.getState(sourceRand, placePos), 2);
         placePos.move(Direction.NORTH);
-        this.setBlock(level, placePos, dirt);
+        level.setBlock(placePos, treeConfig.dirt_provider.getState(sourceRand, placePos), 2);
         placePos.move(Direction.WEST);
-        this.setBlock(level, placePos, dirt);
+        level.setBlock(placePos, treeConfig.dirt_provider.getState(sourceRand, placePos), 2);
         placePos.move(Direction.SOUTH);
-        this.setBlock(level, placePos, dirt);
+        level.setBlock(placePos, treeConfig.dirt_provider.getState(sourceRand, placePos), 2);
 
-        // place leaf blocks
+        int l1 = sourceRand.nextInt(2);  // TODO: do these constants need to be config VARIABLES?
+        int j2 = 1;
+        boolean flag1 = false;
+
+        // place leaves.
         BlockState bsLeaves = treeConfig.foliage_provider.getState(sourceRand, pos);
-        generateCanopy(level, sourceRand, pos, canopy_radius, canopy_depth, bsLeaves);
+        for (int i3 = 0; i3 <= canopy_depth; i3++)
+        {
+            final int k3 = pos.getY() + height - i3;
+            for (int i4 = pos.getX() - l1; i4 <= pos.getX() + l1; i4++)
+            {
+                final int k4 = i4 - pos.getX();
+                for (int l4 = pos.getZ() - l1; l4 <= pos.getZ() + l1; l4++)
+                {
+                    final int i5 = l4 - pos.getZ();
+                    if (Math.abs(k4) != l1 || Math.abs(i5) != l1 || l1 <= 0)
+                    {
+                        BlockPos newpos = new BlockPos(i4, k3, l4);
+                        if (TreeFeature.validTreePos(level, newpos))
+                        {
+                            level.setBlock(newpos, bsLeaves, 2);
+                        }
+                        newpos = new BlockPos(i4 - 1, k3, l4);
+                        if (TreeFeature.validTreePos(level, newpos))
+                        {
+                            level.setBlock(newpos, bsLeaves, 2);
+                        }
+
+                        newpos = new BlockPos(i4, k3, l4 - 1);
+                        if (TreeFeature.validTreePos(level, newpos))
+                        {
+                            level.setBlock(newpos, bsLeaves, 2);
+                        }
+
+                        newpos = new BlockPos(i4 - 1, k3, l4 - 1);
+                        if (TreeFeature.validTreePos(level, newpos))
+                        {
+                            level.setBlock(newpos, bsLeaves, 2);
+                        }
+                    } // end-if
+                } // end-for l4
+            } // end-for i4
+
+            if (l1 >= j2)
+            {
+                l1 = flag1 ? 1 : 0;
+                flag1 = true;
+
+                if (++j2 > canopy_radius)
+                {
+                    j2 = canopy_radius;
+                }
+            }
+            else
+            {
+                l1++;
+            }
+        } // end-for i3
 
         // place trunk blocks
         final int j3 = sourceRand.nextInt(3);
-        generateBranches(level, sourceRand, pos, actual_height - j3, 0.0D);
 
-        return true;
-    } // end place()
-
-    /**
-     * Actually place the branches.
-     *
-     * @param world     current LevalAccessor
-     * @param rand      current RandomSource
-     * @param pos Starting position of branches
-     * @param height    canopy height
-     * @param radius    canopy radius
-     */
-    @Override public boolean generateBranches(LevelAccessor world, RandomSource rand, BlockPos pos, int height,
-                                              double radius)
-    {
-        for (int l3 = 0; l3 < height; l3++)
+        for (int l3 = 0; l3 < height - j3; l3++)
         {
             //final Block block = world.getBlock(x, y + l3, z);
             BlockPos se_pos = new BlockPos(pos.getX(), pos.getY() + l3, pos.getZ());
@@ -117,88 +157,13 @@ public class MegaFirTreeFeature extends EBBaseTreeFeature
                 BlockPos nw_pos = new BlockPos(pos.getX() - 1, pos.getY() + l3, pos.getZ() - 1);
 
                 // TODO revise for quarter blocks
-                this.setBlock(level, se_pos, treeConfig.trunk_provider.getState(sourceRand, se_pos));
-                this.setBlock(level,sw_pos, treeConfig.trunk_provider.getState(sourceRand, sw_pos));
-                this.setBlock(level,ne_pos, treeConfig.trunk_provider.getState(sourceRand, ne_pos));
-                this.setBlock(level,nw_pos, treeConfig.trunk_provider.getState(sourceRand, nw_pos));
+                level.setBlock(se_pos, treeConfig.trunk_provider.getState(sourceRand, se_pos), 2);
+                level.setBlock(sw_pos, treeConfig.trunk_provider.getState(sourceRand, sw_pos), 2);
+                level.setBlock(ne_pos, treeConfig.trunk_provider.getState(sourceRand, ne_pos), 2);
+                level.setBlock(nw_pos, treeConfig.trunk_provider.getState(sourceRand, nw_pos), 2);
             } // end-if
         } // end-for l3
         return true;
-    } // end generateBranches()
-
-    /**
-     * Generate the tree's canopy.
-     *
-     * @param world  LevelAccessor
-     * @param rand   a RandomSource
-     * @param pos    BlockPos of center of canopy
-     * @param radius canopy radius
-     * @param height canopy height
-     * @param leaves leaf BlockState
-     * @return true if successful, false if not.
-     */
-    @Override public void generateCanopy(LevelAccessor world, RandomSource rand, BlockPos pos, double radius,
-                                         int height, BlockState leaves)
-    {
-        int l1 = sourceRand.nextInt(2);  // TODO: do these constants need to be config VARIABLES?
-        int j2 = 1;
-        boolean flag1 = false;
-        BlockPos.MutableBlockPos newpos = new BlockPos.MutableBlockPos();
-
-        // place leaves.
-        for (int i3 = 0; i3 <= height; i3++)
-        {
-            final int k3 = pos.getY() + actual_height - i3;
-            for (int i4 = pos.getX() - l1; i4 <= pos.getX() + l1; i4++)
-            {
-                final int k4 = i4 - pos.getX();
-                for (int l4 = pos.getZ() - l1; l4 <= pos.getZ() + l1; l4++)
-                {
-                    final int i5 = l4 - pos.getZ();
-                    if (Math.abs(k4) != l1 || Math.abs(i5) != l1 || l1 <= 0)
-                    {
-                        newpos.set(i4, k3, l4);
-                        if (TreeFeature.validTreePos(level, newpos))
-                        {
-                            this.setBlock(level, newpos, leaves);
-                        }
-                        newpos.set(i4 - 1, k3, l4);
-                        if (TreeFeature.validTreePos(level, newpos))
-                        {
-                            this.setBlock(level, newpos, leaves);
-                        }
-
-                        newpos.set(i4, k3, l4 - 1);
-                        if (TreeFeature.validTreePos(level, newpos))
-                        {
-                            this.setBlock(level, newpos, leaves);
-                        }
-
-                        newpos.set(i4 - 1, k3, l4 - 1);
-                        if (TreeFeature.validTreePos(level, newpos))
-                        {
-                            this.setBlock(level, newpos, leaves);
-                        }
-                    } // end-if
-                } // end-for l4
-            } // end-for i4
-
-            if (l1 >= j2)
-            {
-                l1 = flag1 ? 1 : 0;
-                flag1 = true;
-
-                if (++j2 > radius)
-                {
-                    j2 = (int) radius;
-                }
-            }
-            else
-            {
-                l1++;
-            }
-        } // end-for i3
-
-    } // end generateCanopy
+    } // end place()
 
 } // end-class
